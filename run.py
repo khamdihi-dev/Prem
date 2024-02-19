723353e1439db4444065bab0c9362f71baa35244
KhamdihiXD <118111949+khamdihi-dev@users.noreply.github.com>
2024-02-19 16:36:51 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,22 @@
+import os
+
+try:
+    import platform
+    import wget,sys
+except:
+    os.system('pip install wget')
+
+bit = platform.architecture()[0]
+if '64' in str(bit):
+   os.sytem('git pull')
+   if os.path.isfile('menu.py') is True:
+      from menu import ListTools as runing
+      exit(runing())
+   else:
+      exit('\nFile Menu Tidak Tersedia.')
+else:
+   os.system('wget https://raw.githubusercontent.com/khamdihi-dev/Prem/main/data/ibrut.zip')
+   if os.path.isfile('ibrut.zip') is True:
+      exit(os.system(f'unzip ibrut.zip;python {sys.argv[0]}'))
+   else:
+      exit('\nDownload gagal silahkan coba lagi..')
