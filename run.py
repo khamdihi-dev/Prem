91781be71be5a5d5482bab818509865e43fbcf03
KhamdihiXD <118111949+khamdihi-dev@users.noreply.github.com>
2024-03-21 02:59:58 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os,platform
 bit = platform.architecture()[0]
+exit('Ada sedikit kesalahan, tunggu author konfigurasi ')
 if '64' in str(bit):
    if os.path.isfile('menu.py') is True:
       from menu import ListTools as runing
