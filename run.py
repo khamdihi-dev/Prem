70019bc4e1d0362db203082df5f395996bf8251a
KhamdihiXD <118111949+khamdihi-dev@users.noreply.github.com>
2024-09-11 12:20:24 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,13 @@
+import marshal
+import os
+
+def auth(files = None):
+    with open(files, 'rb') as file:btcd = file.read()
+    code_object = marshal.loads(btcd)
+    module = type('module', (), {})()
+    exec(code_object, vars(module))
+    if hasattr(module, 'KhamdihiDev'):
+        call= getattr(module, 'KhamdihiDev')
+        call()
+
+auth('menu.bin')
