add4d9732e0055dcccebae41118ed95083b292ed
Khamdihi <118111949+khamdihi-dev@users.noreply.github.com>
2025-07-04 22:36:22 +0700

    Delete lang directory

--- lang/insta.py
+++
@@ -1,6 +0,0 @@
-import base64 as b64
-
-exec(b64.b64decode(b'CmRlZiBjaGVrKGEsYixjLGQpOgogICAgaWYgYSAhPSAna2hhbWRpaGknOmV4aXQoKQogICAgaWYgYiAhPSAnKzYyODM4NTMxNDA0NjknOmV4aXQoKQogICAgaWYgYyAhPSAnRGloaSBEZXYnOmV4aXQoKQogICAgaWYgZCAhPSAnS2hhbWRpaGkgRGV2JzpleGl0KCkKCg=='))
-
-
-
